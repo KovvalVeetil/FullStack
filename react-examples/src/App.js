3f91249cf851df1b49a25ad36d31e7fddbe8094f
Aswathi <akv2595@gmail.com>
2023-09-15 11:18:06 -0700

    Collatz lifecycle method

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,66 +1,35 @@
-// import React, { Component } from 'react';
-// class Thing1 extends Component { 
-//     render() {
-//         const { title, children } = this.props;
-//         return (
-//             <div className="Thing1">
-//                 <h2>{title}</h2>
-//                 {children} 
-//             </div>
-//         );
-//     }
-// }
-
-// class Thing2 extends Component { 
-//     render() {
-//         const { gratitude } = this.props;
-//         return (
-//             <div className="Thing2">
-//             <p>I am grateful for <strong>{ gratitude }</strong></p>
-//             </div>
-//         );
-//     }
-// }
-
-// class App extends Component { 
-//     render() {
-
-//         return (
-//             <div className="App">
-//                 <p>FRAP applications are fun and powerful</p> 
-//                 <Thing1 title="Grateful">Today...</Thing1>
-//                 <Thing2 gratitude="hot coffee" />
-//                 <Thing2 gratitude="bug-free code" />
-//             </div> 
-//         );
-//     }
-// }
-// export default App;
-
 import React, { Component } from 'react';
-class Collatz extends Component { render() {
-    const { number } = this.props;
-    // WARNING: This code reruns with every rendering
-    let x = number;
-    let outputString = x.toString();
-    while(typeof x == 'number' && x !== 1) {
-        if ((x % 2) === 1) { x = 3*x + 1;
-        } else {
-            x = x/2;
+class Collatz extends Component { 
+    constructor(props) {
+        super(props);
+        let x = this.props.number;
+        let outputString = x.toString();
+        while (typeof x == 'number' && x !== 1) {
+            if ((x % 2) === 1) { x = 3*x + 1;
+            }
+            else {
+                 x = x/2;
+            };
+            outputString += ' ' + x; 
         };
-        outputString += ' ' + x; };
+        this.outputString = outputString; 
+    }
+    render() {
+        const { number } = this.props;
+        const outputString = this.outputString;
         return (
-            <div className='Collatz'>
+            <div className="Collatz">
                 <h2>Collatz Sequence for {number}</h2>
-                <p>{ outputString }</p>
+                <p>{ outputString }</p> 
             </div>
         );
     }
 }
 
-class App extends Component {
+class App extends Component { 
     render() {
-        const number = 17; const wikilink = 'https://en.wikipedia.org/wiki/Collatz_conjecture'
+        const number = 17; 
+        const wikilink ='https://en.wikipedia.org/wiki/Collatz_conjecture';
         return (
             <div className="App">
                 <h1>Collatz Conjecture</h1>
@@ -75,5 +44,4 @@ class App extends Component {
         );
     }
 }
-
 export default App;
