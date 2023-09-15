0f59d688369364f1bff732f047ad68985e99e015
Aswathi <akv2595@gmail.com>
2023-09-15 15:48:59 -0700

    collatz_styling

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,47 +1,82 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 class Collatz extends Component { 
     constructor(props) {
         super(props);
         let x = this.props.number;
-        let outputString = x.toString();
+        let outputArr = [x];
         while (typeof x == 'number' && x !== 1) {
-            if ((x % 2) === 1) { x = 3*x + 1;
+            if ((x % 2) === 1) {
+                 x = 3*x + 1;
             }
             else {
                  x = x/2;
             };
-            outputString += ' ' + x; 
+            outputArr.push(x); 
         };
-        this.outputString = outputString; 
+        this.outputArr = outputArr; 
     }
     render() {
         const { number } = this.props;
-        const outputString = this.outputString;
+        const outputArr = this.outputArr;
         return (
             <div className="Collatz">
                 <h2>Collatz Sequence for {number}</h2>
-                <p>{ outputString }</p> 
+                <ul>
+                {
+                    outputArr.map((x,ind)=>{return <SpecialListItem key ={ind}>{x}
+                    </SpecialListItem>;})
+                }
+                </ul>
             </div>
         );
     }
 }
-
+const GridDiv = styled.div`
+        display: grid;
+        grid-template-columns: 30% 70%;
+        grid-template-rows: auto;
+        grid-gap: 10px;
+        background-color: #eee;
+        color: black;
+        margin: 20px;
+`;
+const LeftColumn = styled.div` 
+        grid-column: 1;
+        margin: 0.8em;
+`;
+const RightColumn = styled.div` 
+        grid-column: 2;
+        margin: 0.8em;
+`;
+const EmphaticPar = styled.p`
+        font-style: oblique;
+`;
+const VeryEmphaticPar = styled(EmphaticPar)` font-size: 150%;
+`;
+const SpecialListItem = styled.li`
+        font-weight: bold;
+        font-size: 90%;
+        padding: 2px;
+        list-style-type: ${props => props.listStyleType || "square"};
+`;
 class App extends Component { 
     render() {
         const number = 17; 
         const wikilink ='https://en.wikipedia.org/wiki/Collatz_conjecture';
         return (
-            <div className="App">
-                <h1>Collatz Conjecture</h1>
-                    <p>The <a href={wikilink}>Collatz Conjecture</a>&nbsp;
-                    states that for any positive integer, <i>x</i>,
-                    repeatedly applying <i>3x + 1</i> if odd and 
-                    <i>x/2</i> if even will eventually lead to 1.</p>
-                <p>No one knows if this is true.</p>
-                    
+        <GridDiv>
+        <LeftColumn>
+            <h1>Collatz Conjecture</h1>
+                <p>The <a href={wikilink}>Collatz Conjecture </a>
+                states that for any positive integer, <i>x</i>, repeatedly applying <i>3x + 1</i> if odd
+                and <i>x/2</i> if even will eventually lead to 1.</p>
+                <VeryEmphaticPar>No one knows if this is true.</VeryEmphaticPar> 
+        </LeftColumn>
+        <RightColumn>
                 <Collatz number={number} /> 
-            </div>
-        );
+        </RightColumn>
+        </GridDiv> );
     }
 }
 export default App;
