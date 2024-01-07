999272301334dab84d0172da7992e63278d78473
Aswathi <akv2595@gmail.com>
2024-01-07 11:30:20 +0530

    updating passwords

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,83 +1,46 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { GlobalStyle } from './fonts';
-class Collatz extends Component { 
+import React, {Component} from 'react'; 
+import './App.css';
+
+class App extends Component {
+    static allowedChars = /[a-zA-Z0-9\- .:=;!%]/; 
     constructor(props) {
         super(props);
-        let x = this.props.number;
-        let outputArr = [x];
-        while (typeof x == 'number' && x !== 1) {
-            if ((x % 2) === 1) {
-                 x = 3*x + 1;
+        this.state = {password:'', passwordComment:null};
+        this.updatePassword = this.updatePassword.bind(this); 
+        this.logIt = this.logIt.bind(this);
+        }
+    logIt(event) {
+        event.preventDefault();
+        console.log('submit button clicked with password ' + this.state.password); }
+    updatePassword(event) {
+        // drop unallowable characters
+        const newPassword = event.target.value.split('').filter(x => x.match(App.allowedChars)).join('');
+        
+        let comment;
+        if (newPassword.length < 6) {
+            comment = <p>Minimum length is 6 characters</p>; 
+            } 
+        else if (newPassword.length < 10) {
+            comment = <p>Weak Password &mdash; longer passwords are more secure</p>;} 
+        else {
+            comment = <p>Strong Password</p>;
             }
-            else {
-                 x = x/2;
-            };
-            outputArr.push(x); 
-        };
-        this.outputArr = outputArr; 
+        this.setState({password: newPassword, passwordComment: comment}); 
     }
-    render() {
-        const { number } = this.props;
-        const outputArr = this.outputArr;
+    render() { 
         return (
-            <div className="Collatz">
-                <h2>Collatz Sequence for {number}</h2>
-                <ul>
-                {
-                    outputArr.map((x,ind)=>{return <SpecialListItem key ={ind}>{x}
-                    </SpecialListItem>;})
-                }
-                </ul>
-            </div>
+            <div>
+            <h1>Registration Form</h1> 
+            <form>
+                Password&nbsp;
+                <input type="password" value={this.state.password}
+                    onChange={this.updatePassword}/>
+                <br/>
+                { this.state.passwordComment }
+                <button onClick={this.logIt}>Register</button>
+                </form> 
+                </div>
         );
-    }
-}
-const GridDiv = styled.div`
-        display: grid;
-        grid-template-columns: 30% 70%;
-        grid-template-rows: auto;
-        grid-gap: 10px;
-        background-color: #eee;
-        color: black;
-        margin: 20px;
-`;
-const LeftColumn = styled.div` 
-        grid-column: 1;
-        margin: 0.8em;
-`;
-const RightColumn = styled.div` 
-        grid-column: 2;
-        margin: 0.8em;
-`;
-const EmphaticPar = styled.p`
-        font-style: oblique;
-`;
-const VeryEmphaticPar = styled(EmphaticPar)` font-size: 150%;
-`;
-const SpecialListItem = styled.li`
-        font-weight: bold;
-        font-size: 90%;
-        padding: 2px;
-        list-style-type: ${props => props.listStyleType || "square"};
-`;
-class App extends Component { 
-    render() {
-        const number = 17; 
-        const wikilink ='https://en.wikipedia.org/wiki/Collatz_conjecture';
-        return (
-        <GridDiv>
-        <LeftColumn>
-            <h1>Collatz Conjecture</h1>
-                <p>The <a href={wikilink}>Collatz Conjecture </a>
-                states that for any positive integer, <i>x</i>, repeatedly applying <i>3x + 1</i> if odd
-                and <i>x/2</i> if even will eventually lead to 1.</p>
-                <VeryEmphaticPar>No one knows if this is true.</VeryEmphaticPar> 
-        </LeftColumn>
-        <RightColumn>
-                <Collatz number={number} /> 
-        </RightColumn>
-        </GridDiv> );
     }
 }
 export default App;
