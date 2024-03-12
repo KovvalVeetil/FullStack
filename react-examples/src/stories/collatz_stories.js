8b208ce4ee905aadf1a98c49ec677e08ada369fa
Aswathi <akv2595@gmail.com>
2024-03-12 17:30:21 -0400

    storybook_intro

---
+++ react-examples/src/stories/collatz_stories.js
@@ -0,0 +1,20 @@
+// collatz.stories.js
+import React from 'react';
+import { fn } from '@storybook/test';
+import { Collatz } from '../App.js';
+
+export default {
+    component: Collatz,
+    title: 'Collatz sequence',
+};
+
+export const for8 = () => <Collatz number={8} />;
+for8.story = { name: 'For 8' };
+
+export const for101 = () => <Collatz number={101} />;
+for101.story = { name: 'For 101' };
+
+export const seeMultiple = () => <div>
+        { [ 17, 22, 57 ].map(x => <Collatz number={x} />) }
+    </div>;
+seeMultiple.story = { name: 'See multiple' };
